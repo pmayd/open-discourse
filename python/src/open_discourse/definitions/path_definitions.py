91ee5982f5df1776414955914bc7ba8da3d2c5c3
erwee6 <161755440+erwee6@users.noreply.github.com>
2024-12-20 00:00:52 +0100

    change after pr review

--- python/src/open_discourse/definitions/path_definitions.py
+++ python/src/open_discourse/definitions/path_definitions.py
@@ -1,27 +1,27 @@
 from pathlib import Path
 
-# ROOT DIR _________________________________________________________________________________________
+# ROOT DIR _____________________________________________________________________________
 ROOT_DIR = (Path(__file__) / "../../../..").resolve()
 
-# LOGS DIR _________________________________________________________________________________________
+# LOGS DIR _____________________________________________________________________________
 LOGS_DIR = ROOT_DIR / "logs"
 
-# DATA _____________________________________________________________________________________________
+# DATA _________________________________________________________________________________
 DATA = ROOT_DIR / "data"
 DATA_RAW = DATA / "01_raw"
 DATA_CACHE = DATA / "02_cached"
 DATA_FINAL = DATA / "03_final"
 FINAL = DATA / "03_final"
 
-# MP_BASE_DATA ___________________________________________________________________________________
+# MP_BASE_DATA _________________________________________________________________________
 MP_BASE_DATA = DATA_RAW / "MP_BASE_DATA" / "MDB_STAMMDATEN.XML"
 
-# RAW ______________________________________________________________________________________________
+# RAW __________________________________________________________________________________
 RAW_ZIP = DATA_RAW / "zip"
 RAW_XML = DATA_RAW / "xml"
 RAW_TXT = DATA_RAW / "txt"
 
-# SPEECH CONTENT ___________________________________________________________________________________
+# SPEECH CONTENT _______________________________________________________________________
 SPEECH_CONTENT = DATA_CACHE / "speech_content"
 SPEECH_CONTENT_STAGE_01 = SPEECH_CONTENT / "stage_01"
 SPEECH_CONTENT_STAGE_02 = SPEECH_CONTENT / "stage_02"
@@ -29,7 +29,7 @@ SPEECH_CONTENT_STAGE_03 = SPEECH_CONTENT / "stage_03"
 SPEECH_CONTENT_STAGE_04 = SPEECH_CONTENT / "stage_04"
 SPEECH_CONTENT_FINAL = SPEECH_CONTENT / "final"
 
-# CONTRIBUTIONS_EXTENDED ___________________________________________________________________________
+# CONTRIBUTIONS_EXTENDED _______________________________________________________________
 CONTRIBUTIONS_EXTENDED = DATA_CACHE / "contributions_extended"
 CONTRIBUTIONS_EXTENDED_STAGE_01 = CONTRIBUTIONS_EXTENDED / "stage_01"
 CONTRIBUTIONS_EXTENDED_STAGE_02 = CONTRIBUTIONS_EXTENDED / "stage_02"
@@ -39,25 +39,25 @@ CONTRIBUTIONS_EXTENDED_STAGE_04 = CONTRIBUTIONS_EXTENDED / "stage_04"
 CONTRIBUTIONS_EXTENDED_FINAL = CONTRIBUTIONS_EXTENDED / "final"
 
 
-# POLITICIANS ______________________________________________________________________________________
+# POLITICIANS __________________________________________________________________________
 POLITICIANS = DATA_CACHE / "politicians"
 POLITICIANS_STAGE_01 = POLITICIANS / "stage_01"
 POLITICIANS_STAGE_02 = POLITICIANS / "stage_02"
 POLITICIANS_FINAL = POLITICIANS / "final"
 
-# FACTIONS _________________________________________________________________________________________
+# FACTIONS _____________________________________________________________________________
 FACTIONS = DATA_CACHE / "factions"
 FACTIONS_STAGE_01 = FACTIONS / "stage_01"
 FACTIONS = FACTIONS / "stage_02"
 
 FACTIONS_FINAL = FACTIONS / "final"
 
-# CONTRIBUTIONS_SIMPLIFIED _________________________________________________________________________
+# CONTRIBUTIONS_SIMPLIFIED _____________________________________________________________
 CONTRIBUTIONS_SIMPLIFIED = FINAL
 
-# ELECTORAL_TERMS __________________________________________________________________________________
+# ELECTORAL_TERMS ______________________________________________________________________
 ELECTORAL_TERMS = FINAL
 
 
-# TOPIC_MODELLING __________________________________________________________________________________
+# TOPIC_MODELLING ______________________________________________________________________
 TOPIC_MODELLING = DATA_CACHE / "topic_modelling"
