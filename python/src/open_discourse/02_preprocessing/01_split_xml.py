fb39eb858f836cdef764a64256eb87f3ae7465a4
erwee6 <161755440+erwee6@users.noreply.github.com>
2024-12-20 16:59:26 +0100

    renamed logger setup

--- python/src/open_discourse/02_preprocessing/01_split_xml.py
+++ python/src/open_discourse/02_preprocessing/01_split_xml.py
@@ -5,12 +5,12 @@ Iterate through entire data or select one term or one session
 import logging
 
 import open_discourse.definitions.path_definitions as path_definitions
-from open_discourse.helper_functions.logging_config import setup_logger
+from open_discourse.helper_functions.logging_config import setup_and_get_logger
 from open_discourse.specific_functions.functions_step02_func01 import (
     iterate_preprocessing_completed_terms,
 )
 
-logger = setup_logger(__file__, logging.DEBUG)
+logger = setup_and_get_logger(__file__, logging.DEBUG)
 logger.info("Script 02_01 starts")
 
 # input directory
