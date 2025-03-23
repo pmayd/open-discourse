57a4e1f84faa73cb77591a64e87da8a830c0b3d0
tim <tfangmeyer@gmail.com>
2025-03-23 08:09:37 +0100

    Fix formatting and linting issues

--- python/src/open_discourse/helper/constants.py
+++ python/src/open_discourse/helper/constants.py
@@ -1,8 +1,8 @@
 ADDITIONAL_FACTIONS = [
     "Südschleswigscher Wählerverband",  # A regional party in Germany
-    "Gast",                             # Represents guest speakers or members
-    "Gruppe Nationale Rechte",          # A group with national right-wing affiliations
-    "Deutsche Soziale Union",           # A social union in Germany
+    "Gast",  # Represents guest speakers or members
+    "Gruppe Nationale Rechte",  # A group with national right-wing affiliations
+    "Deutsche Soziale Union",  # A social union in Germany
 ]
 
 # Dictionary mapping full faction names to their abbreviations
