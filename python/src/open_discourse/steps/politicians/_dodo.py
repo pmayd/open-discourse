1d2e4f2f9c12ed7512b288c361dcdecfe62565dc
Tim Fangmeyer <tfangmeyer@gmail.com>
2025-04-05 15:15:18 +0200

    Update _dodo.py to properly integrate refactored code with existing pipeline

--- python/src/open_discourse/steps/politicians/_dodo.py
+++ python/src/open_discourse/steps/politicians/_dodo.py
@@ -13,7 +13,37 @@ TARGET_TASK2 = path.POLITICIANS_STAGE_01 / "04_02.done"
 TARGET_TASK3 = path.DATA_FINAL / "04_03.done"
 
 
-# Empty task function for compatibility
+# Task function for backward compatibility
 def task_04_politicians():
     """Processes politician data by merging MP information with faction IDs and scraped data."""
-    return []
+    
+    # Sub-tasks to match the old naming convention expected by other tasks
+    yield {
+        'name': 'scrape_mgs',
+        'actions': [
+            # This is a wrapper that calls the new task
+            'python -m open_discourse.steps.politicians.scrape_mgs'
+        ],
+        'targets': [TARGET_TASK2],
+        'verbosity': 2,
+    }
+    
+    yield {
+        'name': 'add_faction_id',
+        'actions': [
+            'python -m open_discourse.steps.politicians.add_faction_id_to_mps'
+        ],
+        'file_dep': [TARGET_TASK2],
+        'targets': [TARGET_TASK1],
+        'verbosity': 2,
+    }
+    
+    yield {
+        'name': 'merge',
+        'actions': [
+            'python -m open_discourse.steps.politicians.merge_politicians'
+        ],
+        'file_dep': [TARGET_TASK1, TARGET_TASK2],
+        'targets': [TARGET_TASK3],
+        'verbosity': 2,
+    }
