323dafae63a96774c2752fc08228ebf8bbc2360f
Tim Fangmeyer <tfangmeyer@gmail.com>
2025-04-05 15:50:22 +0200

    Fix formatting with ruff format and remove unused merge_politicians.py

--- python/src/open_discourse/steps/politicians/_dodo.py
+++ python/src/open_discourse/steps/politicians/_dodo.py
@@ -16,34 +16,30 @@ TARGET_TASK3 = path.DATA_FINAL / "04_03.done"
 # Task function for backward compatibility
 def task_04_politicians():
     """Processes politician data by merging MP information with faction IDs and scraped data."""
-    
+
     # Sub-tasks to match the old naming convention expected by other tasks
     yield {
-        'name': 'scrape_mgs',
-        'actions': [
+        "name": "scrape_mgs",
+        "actions": [
             # This is a wrapper that calls the new task
-            'python -m open_discourse.steps.politicians.scrape_mgs'
+            "python -m open_discourse.steps.politicians.scrape_mgs"
         ],
-        'targets': [TARGET_TASK2],
-        'verbosity': 2,
+        "targets": [TARGET_TASK2],
+        "verbosity": 2,
     }
-    
+
     yield {
-        'name': 'add_faction_id',
-        'actions': [
-            'python -m open_discourse.steps.politicians.add_faction_id_to_mps'
-        ],
-        'file_dep': [TARGET_TASK2],
-        'targets': [TARGET_TASK1],
-        'verbosity': 2,
+        "name": "add_faction_id",
+        "actions": ["python -m open_discourse.steps.politicians.add_faction_id_to_mps"],
+        "file_dep": [TARGET_TASK2],
+        "targets": [TARGET_TASK1],
+        "verbosity": 2,
     }
-    
+
     yield {
-        'name': 'merge',
-        'actions': [
-            'python -m open_discourse.steps.politicians.merge'
-        ],
-        'file_dep': [TARGET_TASK1, TARGET_TASK2],
-        'targets': [TARGET_TASK3],
-        'verbosity': 2,
+        "name": "merge",
+        "actions": ["python -m open_discourse.steps.politicians.merge"],
+        "file_dep": [TARGET_TASK1, TARGET_TASK2],
+        "targets": [TARGET_TASK3],
+        "verbosity": 2,
     }
