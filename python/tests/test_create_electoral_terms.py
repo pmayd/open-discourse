a094da0d1fd8cc7ff0f7cb9978b52dea601471b7
erwee6 <161755440+erwee6@users.noreply.github.com>
2025-04-08 14:52:13 +0200

    refactored create_electoral_terms

--- python/tests/test_create_electoral_terms.py
+++ python/tests/test_create_electoral_terms.py
@@ -3,11 +3,35 @@ import pytest
 from open_discourse.helper.create_electoral_terms import create_electoral_terms
 
 
-def test_create_electoral_terms():
+@pytest.mark.parametrize(
+    "term, expected",
+    [
+        (
+            12,
+            {
+                "start_date": "1990-12-20",
+                "end_date": "1994-11-09",
+                "number_of_sessions": 243,
+            },
+        ),
+        (
+            20,
+            {
+                "start_date": "2021-10-26",
+                "end_date": "2025-03-24",
+                "number_of_sessions": 214,
+            },
+        ),
+    ],
+)
+def test_create_electoral_terms(term, expected):
     result = create_electoral_terms()
-    assert result[12]["start_date"] == "1990-12-20"
-    assert result[12]["end_date"] == "1994-11-09"
-    assert result[12]["number_of_sessions"] == 243
+    assert result[term]["start_date"] == expected["start_date"]
+    assert result[term]["end_date"] == expected["end_date"]
+    assert result[term]["number_of_sessions"] == expected["number_of_sessions"]
+    # assert result[12]["start_date"] == "1990-12-20"
+    # assert result[12]["end_date"] == "1994-11-09"
+    # assert result[12]["number_of_sessions"] == 243
 
 
 def test_create_electoral_terms_error():
